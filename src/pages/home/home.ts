c2ff8e12c41169f86c51f026fc24d23e159669b5
kennest225 <kennyoulai@gmail.com>
2017-11-28 12:51:48 +0000

    artist List and searchbar added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,19 +16,23 @@ export class HomePage {
   url: string = 'https://zouglou-rest.herokuapp.com/';
 
   constructor(public navCtrl: NavController, public http: Http, public loader: LoadingController, public data: DataProvider) {
+    this.init();
+  }
+
+  ionViewDidLoad() {
+    this.data.getInactiveEvents();
+  }
+
+  init() {
     this.data.getactiveEvents()
       .then(data => {
         this.events = data;
       });
 
-      this.data.getInactiveEvents()
+    this.data.getInactiveEvents()
       .then(data => {
         this.inactiveevents = data;
       });
-  }
-
-  ionViewDidLoad() {
-    this.data.getInactiveEvents();
   }
 
   showDetails(event) {
@@ -37,6 +41,41 @@ export class HomePage {
       this.navCtrl.push(DetailsPage, { 'event': event, 'query': event.place.address.commune });
     } catch (e) {
       console.log(e);
+    }
+  }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+
+      //Filtre événements d'actualités
+      this.events = this.events.filter((item) => {
+        if (item.title.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+          item.place.title.toLowerCase().indexOf(val.toLowerCase()) > -1||
+          item.place.address.commune.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+          item.place.address.quartier.toLowerCase().indexOf(val.toLowerCase()) > -1){
+          return true
+        }
+        return false
+      });
+
+      //Filtre evenements depassés
+      this.inactiveevents = this.inactiveevents.filter((item) => {
+        if (item.title.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+          item.place.title.toLowerCase().indexOf(val.toLowerCase()) > -1||
+          item.place.address.commune.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+          item.place.address.quartier.toLowerCase().indexOf(val.toLowerCase()) > -1){
+          return true
+        }
+        return false
+      });
+
+
+    } else {
+      this.init();
     }
   }
 }
